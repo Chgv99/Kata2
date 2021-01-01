33cdcaf65c24eae30acc8d0d2ae3318f8e759c7d
Christian García Viguera <christiangv99@gmail.com>
2021-01-01 17:41:20 +0000

    Version 3 added

---
+++ Kata2 v3/src/main/kata2.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class kata2 {
+
+    public static void main(String[] args) {
+        int[] data = new int[]{16, 12, 3, 7, 12, 8, 12, 8, 16, 19, 9, 2, 2, 16, 16};
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
+        
+        for (Map.Entry<Integer,Integer> entry : histogr.entrySet()) {
+            System.out.println(entry.getKey() + " ==> " + histogr.get(entry.getKey()));
+        }
+    }
+    
+}
